4597707748e702b18f21d8ec1851d3367f1a9533
Othneil I Drew <codeguydrew@gmail.com>
2018-11-27 22:46:32 -0600

    Add sql categores and certifications

---
+++ _sql/certifications.sql
@@ -0,0 +1,15 @@
+START TRANSACTION;
+
+INSERT INTO `certifications` (`certification_name`) VALUES
+
+('Remote Pilot'),
+('Student Pilot'),
+('Sport Pilot'),
+('Recreational Pilot'),
+('Private Pilot'),
+('Commercial Pilot'),
+('Airline Transport Pilot'),
+('Flight Instructor'),
+('Ground Instructor');
+
+COMMIT;
